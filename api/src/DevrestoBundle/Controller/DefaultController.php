9ba2b004265ccc5c7158e6db52d572e0ed635bce
geo <geoffrey.leborges@epitech.eu>
2017-02-07 14:10:47 +0100

    Envoie du POST via angular

--- api/src/DevrestoBundle/Controller/DefaultController.php
+++ api/src/DevrestoBundle/Controller/DefaultController.php
@@ -6,7 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-
+use DevrestoBundle\Entity\App\User;
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class DefaultController extends Controller
 {
@@ -25,6 +27,10 @@ class DefaultController extends Controller
      */
     public function registerAction(Request $request)
     {
+        if ($request->isXMLHttpRequest()) {
+            return new JsonResponse(array('data' => 'this is a json response'));
+        }
 
+        return new Response('This is not ajax!', 400);
     }
 }
--- front/app.js
+++ front/app.js
@@ -13,12 +13,12 @@ routeApp.config(['$routeProvider',
                 templateUrl: 'views/default/index.html',
                 controller: 'IndexController'
             })
-            .when('/login',{
+            .when('/register',{
                 templateUrl: 'views/default/home.html',
                 controller: 'HomeController'
-            })
-            .otherwise({
-                redirectTo: '/user'
+                // })
+                // .otherwise({
+                //     redirectTo: '/user'
             });
     }
 ]);
@@ -39,18 +39,61 @@ routeAppControllers.controller('IndexController', ['$scope', '$http',
     }
 ]);
 
+// routeAppControllers.controller('HomeController',  ['$scope', '$http',
+//     function($scope , $http){
+//
+//         console.log($scope.allo);
+//
+//         let data = $.param({
+//             fName: $scope.login,
+//         });
+//
+//
+//         let config = {
+//             headers: {
+//                 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+//             }
+//         };
+//
+//         $http.post('http://127.0.0.1:8000/register', data, config)
+//             .then(function (data, status) {
+//                 $scope.PostDataResponse = data;
+//                 console.log(data);
+//             }).catch(function(data,status){
+//             $scope.ResponseDetails = "Data: " + data +
+//                 "<hr />status: " + status ;
+//         })
+//     }
+// ]);
 
-routeAppControllers.controller('HomeController', ['$scope', '$http',
-    function($scope , $http){
-        $http({
-            method: 'GET',
-            url: 'http://127.0.0.1:8000/register'
-        }).then(function successCallback(response) {
-            console.log(response);
-            // if (response.data.length > 0) {
-            //     $scope.message = response.data[0];
-            // }
+routeAppControllers.controller('testController', ['$scope', '$http',function($scope, $http) {
+
+    $scope.update = function () {
+
+        console.log($scope.login);
+
+        var data = {
+            name : $scope.login
+        };
+
+        $http.post("http://localhost:8000/register", {login : $scope.login})
+            .success(function(data, status, headers, config) {
+                //$scope.data = data;
+                console.log(data);
+            }).error(function(data, status, headers, config) {
+                    console.log("http post function");
+            //$scope.status = status;
         });
+
+        // $http.post("http://localhost:8000/register", data)
+        //     .success(function (data) {
+        //         // $scope.PostDataResponse = data;
+        //         console.log(data);
+        //         console.log("http post function");
+        //
+        //     }).error(function(data, status){
+        //         console.log(data);
+        // });
     }
-]);
+}]);
 
