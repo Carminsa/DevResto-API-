a8eba7a4422ef6e569b55558a2329c1831d3431b
geo <geoffrey.leborges@epitech.eu>
2017-02-10 13:44:59 +0100

    add to cart boutton fonctionnel

--- front/controllers/HomeController.js
+++ front/controllers/HomeController.js
@@ -1,15 +1,27 @@
 'use strict';
 
+var cart = [];
+
 routeAppControllers.controller('HomeController', ['$scope', '$http',
+
     function($scope , $http){
+        // var cart = [];
+
         $http({
             method: 'GET',
             url: 'http://127.0.0.1:8000/home'
         }).then(function (data) {
             $scope.data = data.data;
             console.log(data.data);
+
         }).catch(function(){
             console.log('error');
-        })
+        });
+
+        $scope.send = function(id){
+           cart.push(id);
+           console.log(cart);
+        }
     }
 ]);
+
